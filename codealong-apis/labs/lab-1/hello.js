f1759d0261c96231d5b285ae7178f944157bf8f9
Ryan Day <ryan.day@gsa.gov>
2017-10-04 14:00:57 -0500

    Corrected to HTTP

--- codealong-apis/labs/lab-1/hello.js
+++ codealong-apis/labs/lab-1/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://rest-service.guides.spring.io/greeting"
+        url: "http://rest-service.guides.spring.io/greeting"
     }).then(function(data) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
